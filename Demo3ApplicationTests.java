21390b9299b3ef72bee89b83fe220c20a3405cd2
Nagaraja S <Nagaraja.S@XEBIAINDIA.LOCAL>
2021-09-21 11:30:36 +0530

    applications added

---
+++ Demo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
